488a138233db545984f86626dc2da9836994484c
Kokoc1 <haringey.chula@gmail.com>
2022-08-04 15:42:46 +0300

    SHEP

---
+++ app/src/main/java/com/shep/shep/OrdersViewModel.java
@@ -0,0 +1,7 @@
+package com.shep.shep;
+
+import androidx.lifecycle.ViewModel;
+
+public class OrdersViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/shep/shep/SupportViewModel.java
@@ -0,0 +1,7 @@
+package com.shep.shep;
+
+import androidx.lifecycle.ViewModel;
+
+public class SupportViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
